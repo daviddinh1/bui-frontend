3cce206972a57b11d6d432452531b2f96469b5f5
daviddinh1 <daviddinh602680@gmail.com>
2025-08-26 19:07:47 -0500

    created navbar for frontend

---
+++ src/components/button.tsx
@@ -0,0 +1,8 @@
+//for button add effects later
+export default function Button() {
+  return (
+    <div className="border-2 border-slate-600 text-slate-700 hover:bg-blue-50 rounded-lg p-3">
+      <button className="text-base text-slate-700">INVESTOR LOGIN</button>
+    </div>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import Button from "./button";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex justify-around items-center p-3">
+        <div className="text-slate-700 text-xl font-bold">BUI CAPITAL</div>
+        <Button />
+      </div>
+    </>
+  );
+}
